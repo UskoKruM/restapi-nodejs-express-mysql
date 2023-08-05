c80ced981a9a30af626786cb9b2a80da0c1307b3
Fernando Jiménez <99558177+soyfjimenez@users.noreply.github.com>
2023-08-05 20:45:15 +0200

    Update language.routes.js

--- src/routes/language.routes.js
+++ src/routes/language.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { methods as languageController } from "./../controllers/language.controller";
+import { methods as languageController } from "./../controllers/language.controller.js";
 
 const router = Router();
 
