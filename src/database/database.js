1e7064c42a2dbe86fe8fd12dba644c4634078667
Fernando Jiménez <99558177+soyfjimenez@users.noreply.github.com>
2023-08-05 18:55:39 +0200

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,12 @@
 import mysql from "promise-mysql";
-import config from "./../config";
 
 const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password
+    host: "wearekingly.com",
+    database: "dbgactbzrdzo0s",
+    user: "uumm0hivgxvub",
+    password: "KwazuluBulgaria2020"
 });
-
 const getConnection = () => {
     return connection;
 };
-
-module.exports = {
-    getConnection
-};
+export default getConnection;
